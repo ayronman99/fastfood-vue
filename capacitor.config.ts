c0a88bc8764b54c53d82c63a27fc7aa653008777
ayronman99 <itechman32@gmail.com>
2023-04-28 23:25:00 +0800

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'fastfood-vue',
+  webDir: 'dist',
+  bundledWebRuntime: false
+};
+
+export default config;
