2813729f1f48c71b6d960ebad3aa8929291bcfa1
ayronman99 <itechman32@gmail.com>
2023-05-01 10:32:44 +0800

    savepoint for home

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,21 +1,87 @@
 <template>
-    <div class="head-greeting">
-        <img src="#" alt="logo" />
-        <div>
-            <p>
-                Anneoyong, Tzuyu!
-            </p>
-            <p>
-                What do you want to eat?
-            </p>
+    <ion-content>
+        <div class="head-greeting flex justify-evenly p-2">
+            <img :src="FoodApp" alt="logo" />
+            <div class="p-2">
+                <ion-text>
+                    <p class="text-lg font-medium">
+                        Anneoyong, <span class="font-bold">Momo!</span>
+                    </p>
+                </ion-text>
+                <ion-text>
+                    <p class="font-bold text-2xl">
+                        What do you want to eat?
+                    </p>
+                </ion-text>
+            </div>
         </div>
-    </div>
-    <ion-searchbar placeholder="craving for some kimbap, eel and bread?"></ion-searchbar>
+        <ion-searchbar placeholder="craving for some kimbap, eel and bread?"></ion-searchbar>
+        <ion-list class="border-2 border-red-500">
+            <ion-list-header>
+                <div class="flex justify-between items-center">
+                    <h3 class="text-2xl font-bold">Special Offers!</h3>
+                    <ion-item type="button">
+                        <ion-text slot="end">
+                            View All
+                            <ion-icon aria-hidden="true" :icon="chevronForwardOutline"></ion-icon>
+                        </ion-text>
+                    </ion-item>
+                </div>
+            </ion-list-header>
+            <ion-item>
+                <ion-card>
+                    <ion-img />
+                </ion-card>
+            </ion-item>
+        </ion-list>
 
+        <ion-list>
+            <ion-list-header>
+                <h3 class="text-2xl font-bold">Category</h3>
+            </ion-list-header>
+            <ion-item>
+                <ion-card>
+                    <ion-img />
+                    <ion-card-content>
+
+                    </ion-card-content>
+                </ion-card>
+            </ion-item>
+        </ion-list>
+
+        <ion-list>
+            <ion-list-header>
+                <h3 class="text-2xl font-bold">What's New?</h3>
+            </ion-list-header>
+            <ion-item>
+                <ion-card>
+                    <ion-img />
+                    <ion-card-content>
+
+                    </ion-card-content>
+                </ion-card>
+            </ion-item>
+        </ion-list>
+
+        <ion-list>
+            <ion-list-header>
+                <h3 class="text-2xl font-bold">Most Popular</h3>
+            </ion-list-header>
+            <ion-item>
+                <ion-card>
+                    <ion-img />
+                    <ion-card-content>
+
+                    </ion-card-content>
+                </ion-card>
+            </ion-item>
+        </ion-list>
+    </ion-content>
 </template>
 
 
 <script setup lang="ts">
-import { IonSearchbar } from '@ionic/vue';
-
+import { IonSearchbar, IonContent, IonIcon, IonList, IonListHeader, IonLabel, IonCard, IonCardContent, IonImg, IonText, IonItem, } from '@ionic/vue';
+import { chevronForwardOutline } from 'ionicons/icons';
+import FoodApp from "@/icons/FoodApp.svg"
 </script>
